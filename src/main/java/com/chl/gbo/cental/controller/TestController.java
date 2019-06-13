481f5467deafbfec526b3e4666b4a7a733b9f194
BoYang <88411552@suning-sports.com>
2019-06-13 14:07:51 +0800

    add log4j2

--- src/main/java/com/chl/gbo/cental/controller/TestController.java
+++ src/main/java/com/chl/gbo/cental/controller/TestController.java
@@ -3,6 +3,8 @@ package com.chl.gbo.cental.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ import com.chl.gbo.cental.service.BookRepository;
 
 @RestController
 public class TestController {
+
+    Logger log = LogManager.getLogger(TestController.class);
 
     @Autowired
     private Me me;
@@ -34,11 +38,12 @@ public class TestController {
      */
     @RequestMapping(value = {"/how","/what"}, method = RequestMethod.GET)
     public String aggregation(){
-        return "我的名字叫"+me.getName()+"，今年"+me.getAge()+"岁，身高"+me.getTall()+"厘米。";
+        return "我的名2222121232字叫"+me.getName()+"，今年"+me.getAge()+"岁，身高"+me.getTall()+"厘米。";
     }
 
     @GetMapping(value = "/bookmall")
     public List<Book> findAll(){
+        log.info("展示所有的书籍");
         return bookRepository.findAll();
     }
 
